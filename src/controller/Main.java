24d5cd59836c7b22d49d8e7fc63f55f814a11d80
Haein <haeinjo@postech.ac.kr>
2025-02-11 11:57:25 +0900

    refactor: various input validators integrated into getValidIntInput()

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -28,22 +28,22 @@ public class Main {
         Person person = InputValidator.getValidPersonInput(scanner);
 
         // 3. 어린이 여부 -> 어린이면 어린이 강습 레인에 배정 후 프로그램을 빠르게 끝낸다
-        boolean isChild = InputValidator.getIsChild(scanner);
-        if (isChild) {
+        int isChild = InputValidator.getValidIntInput("\n어린이인가요? (Yes: 1, No: 0) ", scanner);
+        if (isChild == 1) {
             poolService.assignChildLane(person, classLanes);
             return;
         }
 
         // 4. 강습 vs 자유 선택
-        int choice = InputValidator.getValidChoice(scanner);
+        int choice = InputValidator.getValidIntInput("\n강습 레인(1) 또는 자유 레인(0)을 선택하세요: ", scanner);
 
-        if (choice == 1){ // 강습 게인 배정
+        if (choice == 1){ // 강습 레인 배정
             poolService.assignClassLane(person, classLanes);
             return;
         }
 
         // 자유 레인 배정
-        boolean useFin = InputValidator.getUseFin(scanner);
+        int useFin = InputValidator.getValidIntInput("\n핀을 사용하시겠습니까? (Yes: 1, No: 0) ", scanner);
         poolService.assignFreeLane(person, freeLanes, useFin);
 
         scanner.close();
